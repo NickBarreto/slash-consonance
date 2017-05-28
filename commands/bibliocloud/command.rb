d3ac37cc99270843693e24b296cc38821a9d2a52
Nick Barreto <nick@canelo.co>
2017-05-28 12:36:11 +0100

    JSON parsing still an issue in create_response

--- commands/bibliocloud/command.rb
+++ commands/bibliocloud/command.rb
@@ -143,7 +143,7 @@ module Bibliocloud
         set_response_text("Bibliocloud has these books with ‘#{text}’ in the title:\n#{response}")
       end
       if data["products"].length == 1
-        add_response_attachment(response)
+        add_response_attachment(response.to_json)
       end
     end
 
